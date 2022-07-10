52bfd7a5de5ab35c85ad910ff0ace74ce069d780
QiLiJazzBebop <lql4@student.unimelb.edu.au>
2022-07-10 19:43:54 +1000

    fix docker config

--- backend/main.py
+++ backend/main.py
@@ -30,4 +30,7 @@ def create_app(test_config=None):
 
     return app
 
-create_app().run()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app = create_app()
+    app.run(host='0.0.0.0',port=port)
